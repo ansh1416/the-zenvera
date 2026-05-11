de55565ab564030b4e5e0d1fc1dbe8c2743c140b
Ansh <anshjoshi1416@gmail.com>
2026-05-11 11:25:13 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import hero from "../assets/hero-main.PNG"
+import hero from "../assets/hero-main.png"
 import { motion, useScroll,useTransform, useSpring } from "framer-motion"
 
 
